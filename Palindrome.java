8117aa33b8bb7a724e2d8e27b9dafe70a5db2b00
Ramya200583 <siddavatamramyasri@gmail.com>
2025-06-02 19:42:33 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Palindrome
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    int temp=n;
+	    int rev=0;
+	    while(n!=0)
+	    {
+	        int rem=n%10;
+	        rev=rev*10+rem;
+	        n=n/10;
+	    }
+	    if(rev==temp)
+	    {
+	        System.out.println("palindrome");
+	    }
+	    else {
+	        System.out.println("not a palindrome");
+	    }
+	    
+	    
+	}
+}
