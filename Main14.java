dada97e7f2ebf3857ab1df7aba937962aa5134a5
Ramya200583 <siddavatamramyasri@gmail.com>
2025-06-03 15:11:21 +0530

    Add files via upload

---
+++ Main14.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main14
+{
+	public static void main(String[] args) {
+	    Queue <Integer> q=new LinkedList<> ();
+	    q.add(1);
+	    q.add(2);
+	    q.add(3);
+	    System.out.println("elements are "+q);
+	    System.out.println(q.remove());
+	    System.out.println("after removing the elements are "+q);
+}
+}
---
+++ reverseUsingStack.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class reverseUsingStack
+{
+	public static void main(String[] args) {
+	    Stack st=new Stack<> ();
+	    Scanner sc=new Scanner(System.in);
+	    String a=sc.nextLine();
+	    for(int i=0;i<a.length();i++)
+	    {
+	        st.push(a.charAt(i));
+	    }
+	    String b="";
+	    for(int i=0;i<a.length();i++)
+	    {
+	        b=b+st.pop();
+	    }
+	    System.out.println(b);
+}
+}
