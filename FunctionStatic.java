e90d2022a46b317ff05432a95b84aed24fab27dd
Ramya200583 <siddavatamramyasri@gmail.com>
2025-06-06 20:37:46 +0530

    Add files via upload

---
+++ FunctionStatic.java
@@ -0,0 +1,13 @@
+public class FunctionStatic 
+{
+    public static void add()
+    {
+        int a=6;
+        int b=4;
+        int c=a+b;
+        System.out.println(c);
+    }
+    public static void main(String[] args){
+        add();
+    }
+}
